eec7dbf9ce0bcdc564f4b18e394a68a84a45560c
Mati-IR <mpabich4@gmail.com>
2023-12-10 17:50:25 +0100

    Implemented display of one of developer's data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 def main():
     print("BogoSort, leader's role: DevOps, leader's github ID: mikoogla")
+    print("BogoSort, role: Developer, github ID: Mati-IR")
 
 if __name__ == "__main__":
     main()
