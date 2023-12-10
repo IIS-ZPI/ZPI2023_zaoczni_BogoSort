e3f3d9306025770ee9c64fb24f1570d9ade16c74
Mati-IR <mpabich4@gmail.com>
2023-12-10 18:23:31 +0100

    Integrated Multiplication function into main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 
+def Multiplication(A: float, B: float) -> float:
+    return A * B
+
 def main():
     print("BogoSort, leader's role: DevOps, leader's github ID: mikoogla")
     print("BogoSort, role: developer, github ID: jacekdebski")
     print("BogoSort, role: tester, github ID: rfilipczak")
     print("BogoSort, role: DevOps, github ID: mikoogla")
     print("BogoSort, role: Developer, github ID: Mati-IR")
+
+    print(Multiplication(2, 2))
 
 if __name__ == "__main__":
     main()
