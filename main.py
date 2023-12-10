17cf8fc1b744f099f54ef2bce36f5727586eb51c
Jacek Debski <jacek.debski@hotmail.com>
2023-12-10 18:01:47 +0100

    second correction of print

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 def main():
     print("BogoSort, leader's role: DevOps, leader's github ID: mikoogla")
-    print("BogoSort, developer, github ID: jacekdebski")
+    print("BogoSort, role: developer, github ID: jacekdebski")
 
 if __name__ == "__main__":
     main()
