0f0cd7ecb0c5660f4de53fed8f7f8011e4465b21
Rafal Filipczak <rfilipczak3@gmail.com>
2023-12-10 17:50:33 +0100

    Print rfilipczak info

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 def main():
     print("BogoSort, leader's role: DevOps, leader's github ID: mikoogla")
+    print("BogoSort, role: tester, github ID: rfilipczak")
 
 if __name__ == "__main__":
     main()
