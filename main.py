839cb12abcab014d594d9cf60b6183b60ea33da4
Jacek Debski <jacek.debski@hotmail.com>
2023-12-10 17:39:07 +0100

    add own role and Github ID

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 def main():
     print("BogoSort, leader's role: DevOps, leader's github ID: mikoogla")
+    print("BogoSort, leader's role: developer, github ID: jacekdebski")
 
 if __name__ == "__main__":
     main()
