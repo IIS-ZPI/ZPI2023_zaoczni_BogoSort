ad292a183d3e8501683a07bcff55987f97cd6403
mikoogla <mikoogla.dev@gmail.com>
2023-12-10 17:51:38 +0100

    add my role

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 def main():
     print("BogoSort, leader's role: DevOps, leader's github ID: mikoogla")
+    print("BogoSort, DevOps, leader's github ID: mikoogla")
 
 if __name__ == "__main__":
     main()
