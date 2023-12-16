b8d0660abbf502714e7490c9515c9d0bbe4f21ff
Jacek Debski <jacek.debski@hotmail.com>
2023-12-16 13:40:05 +0100

    new comment before sample output

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def Addition(A: float, B: float) -> float:
 
 def Difference(A: float, B: float) -> float:
     return A - B
-  
+
 def Division (A: float, B :float) -> float:
     if B == 0:
         print("Error: division by zero")
@@ -21,6 +21,7 @@ def main():
     print("BogoSort, role: DevOps, github ID: mikoogla")
     print("BogoSort, role: Developer, github ID: Mati-IR")
 
+    # sample output:
     print(f"2 + 3 = {Addition(2, 3)}")
     print("3 - 1 = ", Difference(3, 1))
     print(f'2 * 2 = {Multiplication(2, 2)}')
