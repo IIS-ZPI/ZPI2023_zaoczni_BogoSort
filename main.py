59e5c4f4147930970f6520e6b3de03a7b2079872
Jacek Debski <57401153+jacekdebski@users.noreply.github.com>
2023-12-17 16:06:28 +0100

    Feature lab1 zad5/jacekdebski (#19)
    * new comment before sample output
    
    * add a comment above the team info print
    
    * new comment above function definitions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#function defitions
 # operations
 def Addition(A: float, B: float) -> float:
     return A + B
@@ -9,6 +10,7 @@ def Difference(A: float, B: float) -> float:
 
 # parameters: A, B
 # return: A/B
+
 def Division (A: float, B :float) -> float:
     if B == 0:
         # print error
@@ -23,16 +25,19 @@ def Multiplication(A: float, B: float) -> float:
 
 
 def main():
+    # print team info
     # First comment
+    
     print("BogoSort, leader's role: DevOps, leader's github ID: mikoogla")
     print("BogoSort, role: developer, github ID: jacekdebski")
     print("BogoSort, role: tester, github ID: rfilipczak")
     print("BogoSort, role: DevOps, github ID: mikoogla")
     print("BogoSort, role: Developer, github ID: Mati-IR")
 
+    # sample output:
     # test if the functions work correctly
     # Second comment
-
+    
     print(f"2 + 3 = {Addition(2, 3)}")
     print("3 - 1 = ", Difference(3, 1))
     print(f'2 * 2 = {Multiplication(2, 2)}')
